da81a2fabf926238ca02c033add5b018026af168
lugas02 <93799224+lugas02@users.noreply.github.com>
2022-08-07 23:16:52 -0300

    Add files via upload

--- aulaPratica1.c
+++ aulaPratica1.c
@@ -1,7 +1,8 @@
-// Lucas Costa Garcia - M1
+// Lucas Costa Garcia
+// 2022/1 - 22000297 - M1 - ALGORITMOS E ESTRUTURAS DE DADOS I
 
 // Lista 02 - Comandos Condicionais
-// 10. Faça um programa que receba a altura e o sexo de uma pessoa e calcule e mostre seu
+// 10. Faça um programa que receba a altura e o sexo de uma pessoa, calcule e mostre seu
 // peso ideal, utilizando as seguintes fórmulas (onde h corresponde à altura):
 
 // Homens: (72.7 ∗ h) − 58
@@ -21,13 +22,13 @@ int main() {
 
     if ( sexo == 'h' ) {
         pesoIdeal = ( 72.7 * h ) - 58;
-        printf("Seu peso ideal: %.2f kg", pesoIdeal);
     } else {
-    if ( sexo == 'm' ) {                                 // if desnecessário.
+        ( sexo == 'm' ) {
         pesoIdeal = ( 62.1 * h ) - 44.7;
-        printf("Seu peso ideal: %.2f kg", pesoIdeal);    // printf repetido.
         }
     }
+
+    printf("Seu peso ideal: %.2f kg", pesoIdeal);
 
     return 0;
 }

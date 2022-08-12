5e6c681fd249f9009cebba84e785034980954a43
lugas02 <93799224+lugas02@users.noreply.github.com>
2022-08-12 14:15:35 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,29 @@
+//Lista C09 - Exercício 4
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void troca_Inteiro(int *ptrX, int *ptrY) {
+    int temp = *ptrX;
+
+    *ptrX = *ptrX * 2;
+    *ptrY = *ptrY * 2;
+
+    printf("\nSoma do dobro de a e b: %d\n\n", (*ptrX + *ptrY));
+}
+
+int main()
+{
+    int a, b;
+
+    printf("Digite a: ");
+    scanf("%d", &a);
+    printf("Digite b: ");
+    scanf("%d", &b);
+
+    troca_Inteiro(&a, &b);
+
+    printf("a: %d\nb: %d", a, b);
+
+    return 0;
+}
